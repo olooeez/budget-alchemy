8ef27a826e25f5760da0cd4c2d863782ac5766cf
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-08-28 13:18:59 -0300

    Adicionando estrutura básica do projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+import Details from './components/Details/Details'
+import Main from './components/Main/Main'
+import useStyles from './styles'
 
 const App = () => {
+  const classes = useStyles()
+
   return (
-    <div>App</div>
+    <Grid className={classes.grid} container spacing='0px' alignItems='center' justifyContent='center' style={{ height: '100vh' }}>
+      <Grid item xs='12' sm='4'>
+        <Details title='Income' />
+      </Grid>
+      <Grid item xs='12' sm='3'>
+        <Main />
+      </Grid>
+      <Grid item xs='12' sm='4'>
+        <Details title='Expenses' />
+      </Grid>
+    </Grid>
   )
 }
 
